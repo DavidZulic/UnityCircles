8245a86b24d37d29bc0dc522c6a1f123950f91f3
ZulicDavid <batzstudio@gmail.com>
2019-07-05 18:40:07 +0200

    Initial commit

---
+++ CirclesExample.cs
@@ -0,0 +1,31 @@
+﻿/**************************************************************** 
+*   CirclesExample.cs
+*   David Zulic - 2019
+*   MIT License (check included License.md for details)
+*****************************************************************/
+
+using UnityEngine;
+
+public class CirclesExample : MonoBehaviour
+{
+    void Update()
+    {
+        //Draw a circle facing one of global direction, with various parameters
+        DebugExt.DrawCircle(Vector3.zero, Vector3.forward, 0.1f, Color.red);
+        DebugExt.DrawCircle(Vector3.right, Vector3.up, 0.2f, Color.red);
+        DebugExt.DrawCircle(Vector3.right * 2, Vector3.right, 0.4f, Color.red);
+
+        //Draw a circle facing an arbitrary direction
+        Vector3 randomVector = new Vector3(Random.value, Random.value, Random.value);
+        DebugExt.DrawCircle(Vector3.right * 3, randomVector, 0.4f, Color.blue);
+
+        //Draw a sphere facing global forward direction, with various parameters
+        DebugExt.DrawSphere(Vector3.up, 0.01f, Color.magenta);
+        DebugExt.DrawSphere(Vector3.up + Vector3.right, 0.05f, Color.magenta);
+        DebugExt.DrawSphere(Vector3.up + Vector3.right * 2, 0.1f, Color.magenta);
+        DebugExt.DrawSphere(Vector3.up + Vector3.right * 3, 0.5f, Color.magenta);
+
+        //Draw a sphere facing an arbitrary direction
+        DebugExt.DrawSphere(Vector3.up * 3, transform.forward, transform.up, 0.2f, Color.cyan, 32);
+    }
+}
---
+++ DebugExt.cs
@@ -0,0 +1,122 @@
+﻿/**************************************************************** 
+*   DebugExt.cs
+*   David Zulic - 2019
+*   MIT License (check included License.md for details)
+*****************************************************************/
+
+using UnityEngine;
+
+//Debug "extension" class
+public static class DebugExt
+{
+   const int MinSegmentsCount = 8;
+   const int DefaultSegmentsCount = 32;
+   const float DefaultRadius = 1f;
+
+   /// <summary>
+   /// Draw a Debug Circle with a following parameters
+   /// </summary>
+   /// <param name="center">Circle center</param>
+   /// <param name="faceAxis">Circle facing/pivot axis</param>
+   /// <param name="radius">Circle radius</param>
+   /// <param name="color">Circle color</param>
+   /// <param name="segments">Circle arc segments</param>
+   public static void DrawCircle(Vector3 center, Vector3 faceAxis, float radius, Color color, int segments = DefaultSegmentsCount)
+   {
+       segments = segments >= MinSegmentsCount ? segments : MinSegmentsCount;
+
+       Quaternion rotation = Quaternion.FromToRotation(Vector3.forward, faceAxis);
+
+       Vector3[] points = new Vector3[segments];
+
+       float radPerSegment = (360f / (segments - 1)) * Mathf.Deg2Rad;
+
+       for(int i = 0; i < segments; i++)
+       {
+           points[i] = new Vector3(Mathf.Cos(radPerSegment * i), Mathf.Sin(radPerSegment * i), 0) * radius;
+       }
+
+       Vector3[] alignedPoints = new Vector3[segments];
+       Matrix4x4 m = Matrix4x4.Rotate(rotation);
+
+       for (int i = 0; i < segments; i++)
+       {
+           alignedPoints[i] = m.MultiplyPoint3x4(points[i]) + center;
+       }
+
+       for (int i = 1; i < segments; i++)
+       {
+           Debug.DrawLine(alignedPoints[i - 1], alignedPoints[i], color);
+       }
+   }
+
+   /// <summary>
+   /// Draw a Debug Sphere with a following parameters
+   /// </summary>
+   /// <param name="center">Sphere center</param>
+   /// <param name="fwd">Sphere forward vector</param>
+   /// <param name="up">Sphere up vector</param>
+   /// <param name="radius">Sphere radius</param>
+   /// <param name="color">Sphere color</param>
+   /// <param name="segments">Sphere arc segments (per disc/circle)</param>
+   public static void DrawSphere(Vector3 center, Vector3 fwd, Vector3 up, float radius, Color color, int segments = DefaultSegmentsCount)
+   {
+       DrawCircle(center, fwd, radius, color, segments);
+       DrawCircle(center, up, radius, color, segments);
+       DrawCircle(center, Vector3.Cross(fwd, up), radius, color, segments);
+   }
+
+   #region DrawCircle convenience methods
+   public static void DrawCircle(Vector3 center, float radius, Color color, int segments = DefaultSegmentsCount)
+   {
+       DrawCircle(center, Vector3.forward, radius, color, segments);
+   }
+   public static void DrawCircle(Vector3 center, Vector3 faceAxis, float radius)
+   {
+       DrawCircle(center, faceAxis, radius, Color.white, DefaultSegmentsCount);
+   }
+   public static void DrawCircle(Vector3 center, Vector3 faceAxis)
+   {
+       DrawCircle(center, faceAxis, DefaultRadius, Color.white, DefaultSegmentsCount);
+   }
+   public static void DrawCircle(Vector3 center, float radius, int segments = DefaultSegmentsCount)
+   {
+       DrawCircle(center, Vector3.forward, radius, Color.white, segments);
+   }
+   public static void DrawCircle(Vector3 center, float radius)
+   {
+       DrawCircle(center, Vector3.forward, radius, Color.white, DefaultSegmentsCount);
+   }
+   public static void DrawCircle(Vector3 center)
+   {
+       DrawCircle(center, Vector3.forward, DefaultRadius, Color.white, DefaultSegmentsCount);
+   }
+   #endregion
+
+   #region DrawSphere convenience methods
+   public static void DrawSphere(Vector3 center, Vector3 fwd, Vector3 up, float radius)
+   {
+       DrawSphere(center, Vector3.forward, Vector3.up, radius, Color.white, DefaultSegmentsCount);
+   }
+
+   public static void DrawSphere(Vector3 center, Vector3 fwd, Vector3 up)
+   {
+       DrawSphere(center, Vector3.forward, Vector3.up, DefaultRadius, Color.white, DefaultSegmentsCount);
+   }
+
+   public static void DrawSphere(Vector3 center, float radius, Color color, int segments = DefaultSegmentsCount)
+   {
+       DrawSphere(center, Vector3.forward, Vector3.up, radius, color, segments);
+   }
+
+   public static void DrawSphere(Vector3 center, float radius)
+   {
+       DrawSphere(center, Vector3.forward, Vector3.up, radius, Color.white, DefaultSegmentsCount);
+   }
+
+   public static void DrawSphere(Vector3 center)
+   {
+       DrawSphere(center, Vector3.forward, Vector3.up, DefaultRadius, Color.white, DefaultSegmentsCount);
+   }
+   #endregion
+}
